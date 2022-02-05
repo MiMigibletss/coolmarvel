5faf6c31b6c260eff318dbec3d23aa18bd5d9c91
coolmarvel <marvel97@naver.com>
2022-02-06 01:55:17 +0900

    index 변경

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export { default as Navbar } from "./Navbar";
-
 export { default as Homepage } from "./Homepage";
 export { default as Menu } from "./Menu";
 export { default as Port1 } from "./Port1";
